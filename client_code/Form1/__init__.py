05e2ef3859c3bb2d756db504a57805d15b2de6eb
piotr.j.zielinski+anvil@gmail.com <piotr.j.zielinski+anvil@gmail.com>
2024-08-18 18:57:14 +0000

    Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.server
 
 
@@ -9,3 +12,18 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.name_box.text
+    email = self.email_box.text
+    feedback=self.feedback_box.text
+    anvil.server.call('send_feedback', name, email, feedback)
+    Notification("Feedback submitted!").show()
+    self.clear_inputs()
+  
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,5 +1,11 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 import anvil.email
 import anvil.server
+
+
+from datetime import datetime
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -13,16 +19,8 @@ import anvil.server
 #   print("Hello, " + name + "!")
 #   return 42
 #
+@anvil.server.callable
 def send_feedback(name, email, feedback):
   # Send yourself an email each time feedback is submitted
-  anvil.email.send(to="test@mail.test", # Change this to your email address!
-                   subject=f"Feedback from {name}",
-                   text=f"""
-                   
-  A new person has filled out the feedback form!
-
-  Name: {name}
-  Email address: {email}
-  Feedback:
-  {feedback}
-  """)
+  app_tables.feedback.add_row(name=name,email=email,feedback=feedback,created=datetime.now())
+  
